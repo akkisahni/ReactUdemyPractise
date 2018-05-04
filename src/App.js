c27ee3f52fae76d3a2cfd5e440f914cf2e1e2912
Akhil Sahni <asahni5@sapient.com>
2018-05-04 12:04:15 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -16,8 +16,8 @@ class App extends Component {
     };
   }
 
-  showPersons = () =>{
-    this.setState({showPersons:true});
+  togglePersons = () =>{
+    this.setState({showPersons:!this.state.showPersons});
   }
 
   inputChangeHandler = (event, id)=>{
@@ -30,8 +30,20 @@ class App extends Component {
       this.setState({persons:personArray});
   }
 
+    deletePersonHandler = (index) =>{
+        const persons = [...this.state.persons];
+        persons.splice(index,1);
+        this.setState({persons:persons});
+    }
+
   render() {
     const {persons} = this.state;
+    const buttonStyle = {
+      backgroundColor:'#ffffff',
+      color:'#000000',
+      height:'40px',
+      margin:'10px'
+    };
     return (
       <div className="App">
         <header className="App-header">
@@ -39,12 +51,13 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <div className="App-intro">
-          <button onClick={this.showPersons}>Toggle person</button>
+          <button style={buttonStyle} onClick={this.togglePersons}>Toggle person</button>
           {this.state.showPersons &&
-          persons.map((person) => {
+          persons.map((person,index) => {
               return <Person
                   key={person.id}
                   name={person.name}
+                  deletePerson = {()=>{this.deletePersonHandler(index);}}
                   inputChangeHandler={this.inputChangeHandler}
                   age={person.age}
                   id={person.id}/>})
--- src/Person/index.js
+++ src/Person/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './person.css';
 const Person = (props) =>{
 	return(<div className="person">
-			<p> {`hi my name is ${props.name} and my age is ${props.age}`}</p>
+			<p onClick={props.deletePerson}> {`hi my name is ${props.name} and my age is ${props.age}`}</p>
 			<input type="text" value={props.age} onChange={(event)=>{props.inputChangeHandler(event,props.id)}}/>
 		</div>)
 }
